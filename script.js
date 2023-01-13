e0f7b78eb79585371bb8371c65e1cd18089f00d7
Topman-14 <talktotimothy101@gmail.com>
2023-01-13 17:04:10 +0100

    add dark-mode functionality

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 document.querySelector(".container").addEventListener('click', ()=>{
     document.querySelector('.mobile-menu').classList.toggle("menuup");
 })
+
+let darkmode = localStorage.getItem('darkmode');
+
+const toggleDarkmode = document.querySelector('#themeBtn');
+
+const enableDarkMode =() =>{
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'enabled');
+};
+const disableDarkMode =() =>{
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+};  
+
+if (darkmode === 'enabled'){
+    enableDarkMode();
+}
+
+toggleDarkmode.addEventListener('click', ()=>{
+    darkmode = localStorage.getItem('darkmode');
+    if(darkmode !== 'enabled'){
+        enableDarkMode()
+    }
+    else{
+        disableDarkMode()
+    }
+})
